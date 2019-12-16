f5b8958823d5c176fcfa0b7811c7f1834f1ca04c
Ilasciuc Dmitrii <Ilasciuc.Dmitrii@mail.ru>
2019-12-16 23:12:15 +0200

    commit

--- 1-5channel-admin.tv/components/input-find/input-find.ts
+++ 1-5channel-admin.tv/components/input-find/input-find.ts
@@ -1,48 +1,39 @@
 export class InputFind<ItemType> {
 
-    html: {
+    private html: {
         root?: HTMLElement;
         input?: HTMLInputElement;
         valuesContainer?: HTMLElement;
     } = {}
 
-    private valuesList: ItemType[];
-
     constructor(
         element: HTMLElement,
-        valuesList: ItemType[],
-        value: ItemType,
+        private valuesList: ItemType[],
+        private currentValue: ItemType | null,
         private findField: string
     ) {
-        this.valuesList = valuesList;
         this.html.root = element;
         this.html.root.classList.add("input-find");
         this.html.root.innerHTML = "";
         this.html.input = document.createElement("input");
+        this.html.input.value = this.currentValue ? this.currentValue[this.findField] : "";
         this.html.valuesContainer = document.createElement("div");
         this.html.valuesContainer.classList.add("input-find__values-container");
 
         this.html.input.addEventListener("input", this.displayFindRezults.bind(this));
-
         const visibleListenner = (event: MouseEvent) => {
             if (!this.html.root.contains(event.target as HTMLElement)) {
                 document.removeEventListener("mousedown", visibleListenner);
                 this.html.valuesContainer.classList.remove("_visible");
-                this.html.input.value = JSON.parse(this.html.root.dataset.value || "{}")[this.findField] || "";
+                this.html.input.value = this.currentValue ? this.currentValue[this.findField] : "";
             }
         };
-
         this.html.input.addEventListener("focus", () => {
             document.addEventListener("mousedown", visibleListenner)
             this.displayFindRezults();
         });
-
         this.html.valuesContainer.addEventListener("click", this.onSelectValue.bind(this, visibleListenner));
 
-        if (value) {
-            this.html.root.dataset.value = JSON.stringify(value);
-            this.html.input.value = value[this.findField];
-        }
         this.html.root.appendChild(this.html.input);
         this.html.root.appendChild(this.html.valuesContainer);
     }
@@ -53,8 +44,8 @@ export class InputFind<ItemType> {
         });
         if (words.length === 0) return [];
 
-        let rezult = this.valuesList.map((item) => {
-            const newItem = { item, wordsQuantity: 0 };
+        let rezult = this.valuesList.map((item, idx) => {
+            const newItem = { value: item, wordsQuantity: 0, arrIdx: idx };
             for (let word of words) {
                 if ((new RegExp(`^${word}[\\s.,-]+|[\\s.,-]+${word}[\\s.,-]+|[\\s.,-]+${word}$`, "i")).test(item[this.findField])) {
                     newItem.wordsQuantity++
@@ -63,7 +54,7 @@ export class InputFind<ItemType> {
             return newItem;
         }).filter((item) => item.wordsQuantity > 0);
         rezult.sort((item1, item2) => item2.wordsQuantity - item1.wordsQuantity);
-        return rezult.map((item) => item.item);
+        return rezult;
     }
 
     private displayFindRezults() {
@@ -71,8 +62,8 @@ export class InputFind<ItemType> {
         let trueValues = this.find();
         for (let item of trueValues) {
             const htmlItem = document.createElement("div");
-            htmlItem.dataset.value = JSON.stringify(item);
-            htmlItem.innerText = item[this.findField];
+            htmlItem.dataset.valueArrIdx = item.arrIdx + "";
+            htmlItem.innerText = item.value[this.findField];
             htmlItem.classList.add("input-find__values-item");
             this.html.valuesContainer.appendChild(htmlItem);
         }
@@ -84,9 +75,9 @@ export class InputFind<ItemType> {
         document.removeEventListener("mousedown", visibleListenner);
         const element = event.target as HTMLElement;
         if (!element.classList.contains("input-find__values-item")) return;
-        this.html.root.dataset.value = element.dataset.value;
-        this.html.input.value = JSON.parse(element.dataset.value)[this.findField];
+        this.currentValue = this.valuesList[+element.dataset.valueArrIdx];
+        this.html.input.value = this.currentValue[this.findField];
         this.html.valuesContainer.classList.remove("_visible");
-        this.html.root.dispatchEvent(new CustomEvent("change"));
+        this.html.root.dispatchEvent(new CustomEvent("inputFindChange", { detail: { value: this.currentValue } }));
     }
 }
--- 1-5channel-admin.tv/pages/scheldule/scheldule.ts
+++ 1-5channel-admin.tv/pages/scheldule/scheldule.ts
@@ -4,144 +4,235 @@ import { backendAddress } from "../../services/common";
 import { InputFind } from "../../components/input-find/input-find"
 export { };
 
-const comp = document.getElementsByClassName("scheldule-crud-container")[0] as HTMLElement;
-const addBtn = comp.querySelector(".table__btn._add") as HTMLButtonElement;
-const saveBtn = comp.querySelector(".table__btn._save") as HTMLButtonElement;
-const cancelBtn = comp.querySelector(".table__btn._cancel") as HTMLButtonElement;
-const dateInput = comp.getElementsByClassName("date-input")[0] as HTMLInputElement;
-const rowsContainer = comp.getElementsByClassName("table__body")[0] as HTMLElement;
-const rows = rowsContainer.getElementsByClassName("table__row") as HTMLCollectionOf<HTMLElement>;
-let schelduleItems: Scheldule[] = [];
-let programs: Program[] = [];
+class ScheldulePage {
+    html: {
+        root: HTMLElement;
+        addBtn: HTMLButtonElement;
+        saveBtn: HTMLButtonElement;
+        cancelBtn: HTMLButtonElement;
+        dateInput: HTMLInputElement;
+        rowsContainer: HTMLElement;
+    };
 
-addBtn.addEventListener("click", onAddBtnClick);
-cancelBtn.addEventListener("click", onCancelBtnClick);
-saveBtn.addEventListener("click", onSaveBtnClick);
-dateInput.value = new Date().getFullYear() + "-" + new Date().getMonth() + "-" + new Date().getDate();
-dateInput.addEventListener("click", onDateChange);
-onDateChange();
+    programs: Program[] = [];
+    schelduleItems: SchelduleItem[] = [];
 
+    rows: Row[] = [];
 
+    constructor() {
+        const compRoot = document.getElementsByClassName("scheldule-crud-container")[0] as HTMLElement;
+        this.html = {
+            root: compRoot,
+            addBtn: compRoot.querySelector(".table__btn._add") as HTMLButtonElement,
+            saveBtn: compRoot.querySelector(".table__btn._save") as HTMLButtonElement,
+            cancelBtn: compRoot.querySelector(".table__btn._cancel") as HTMLButtonElement,
+            dateInput: compRoot.getElementsByClassName("date-input")[0] as HTMLInputElement,
+            rowsContainer: compRoot.getElementsByClassName("table__body")[0] as HTMLElement,
+        };
 
-function onDateChange() {
-    const pr1 = fetch(backendAddress + `/api/program/getAll.php`).then((resp) => resp.json());
-    const pr2 = fetch(backendAddress + `/api/scheldule/getAll.php?date=` + +new Date(dateInput.value)).then((resp) => resp.json());
-    Promise.all([pr1, pr2]).then((data) => {
-        programs = data[0];
-        schelduleItems = data[1];
-        rowsContainer.innerHTML = "";
-        for (let item of schelduleItems) rowsContainer.insertAdjacentHTML("beforeend", createRow(item));
-        for (let i = 0; i < rows.length; i++) addEventListenersToRow(rows[i]);
-    });
-}
+        this.html.addBtn.addEventListener("click", this.onAddBtnClick.bind(this));
+        this.html.cancelBtn.addEventListener("click", this.onCancelBtnClick.bind(this));
+        this.html.saveBtn.addEventListener("click", this.onSaveBtnClick.bind(this));
+        this.html.dateInput.value = new Date().getFullYear() + "-" + new Date().getMonth() + "-" + new Date().getDate();
+        this.html.dateInput.addEventListener("change", this.onDateChange.bind(this));
+        this.onDateChange();
+    }
 
-function getRowElems(row: HTMLElement) {
-    return {
-        id: row.querySelector(".table__cell._id .table__cell-value") as HTMLElement,
-        timeValue: row.querySelector(".table__cell._time .table__cell-value") as HTMLElement,
-        programNameChangeElem: row.querySelector(".table__cell._program-name .table__cell-change-elem") as HTMLElement,
-        programDurationValue: row.querySelector(".table__cell._program-duration .table__cell-value") as HTMLElement,
+    onDateChange() {
+        if (!this.html.dateInput.value) return;
+        const pr1 = fetch(backendAddress + `/api/program/getAll.php`).then((resp) => resp.json());
+        const pr2 = fetch(
+            backendAddress + `/api/scheldule/getAll.php?date=` + +new Date(this.html.dateInput.value)
+        ).then((resp) => resp.json());
 
-        btnOrderPlus: row.querySelector(".table__btn._order-plus") as HTMLButtonElement,
-        btnOrderMinus: row.querySelector(".table__btn._order-minus") as HTMLButtonElement,
-        btnDelete: row.querySelector(".table__btn._delete") as HTMLButtonElement
+        Promise.all([pr1, pr2]).then((data) => {
+            this.programs = data[0];
+            this.schelduleItems = data[1];
+
+            this.html.rowsContainer.innerHTML = "";
+            for (let item of this.schelduleItems) {
+                item.program = this.programs.find((item2) => item2.id === item.program_id);
+                this.addRow(item);
+            }
+            this.getOrdersTime();
+        });
+    }
+
+    addRow(schelduleItem: SchelduleItem) {
+        this.html.rowsContainer.insertAdjacentHTML("beforeend",
+            `<div class="table__row" data-scheldule-id="${schelduleItem.id}">
+                <div class="table__content-cells-container">
+                    <div class="table__cell _time">
+                        <div class="table__cell-value">${""}</div>
+                    </div>
+                    <div class="table__cell _program-name">
+                        <div class="table__cell-change-elem"></div>
+                    </div>
+                    <div class="table__cell _program-duration">
+                        <div class="table__cell-value">${this.convertDuration(schelduleItem.program.duration).str}</div>
+                    </div>
+                </div>
+                <div class="table__cell _btns">
+                    <button class="pi pi-arrow-up table__btn _order-minus"></button>
+                    <button class="pi pi-arrow-down table__btn _order-plus"></button>
+                    <button class="pi pi-trash table__btn _delete"></button>
+                </div>
+            </div>`
+        );
+        const newRowHtml = this.html.rowsContainer.lastElementChild as HTMLElement;
+        const newRow = {
+            html: {
+                root: newRowHtml,
+                timeValue: newRowHtml.querySelector(".table__cell._time .table__cell-value") as HTMLElement,
+                programNameChangeElem: newRowHtml.querySelector(".table__cell._program-name .table__cell-change-elem") as HTMLElement,
+                programDurationValue: newRowHtml.querySelector(".table__cell._program-duration .table__cell-value") as HTMLElement,
+                btnOrderPlus: newRowHtml.querySelector(".table__btn._order-plus") as HTMLButtonElement,
+                btnOrderMinus: newRowHtml.querySelector(".table__btn._order-minus") as HTMLButtonElement,
+                btnDelete: newRowHtml.querySelector(".table__btn._delete") as HTMLButtonElement
+            },
+            value: schelduleItem
+        } as Row;
+        this.rows.push(newRow);
+
+        new InputFind<Program>(
+            newRow.html.programNameChangeElem,
+            this.programs,
+            schelduleItem.program,
+            "name"
+        );
+        newRow.html.programNameChangeElem.addEventListener("inputFindChange", this.onSelectProgram.bind(this, newRow));
+        newRow.html.btnDelete.addEventListener("click", this.onDeleteBtnClick.bind(this, newRow));
+        newRow.html.btnOrderPlus.addEventListener("click", this.onOrderPlusBtnClick.bind(this, newRow));
+        newRow.html.btnOrderMinus.addEventListener("click", this.onOrderMinusBtnClick.bind(this, newRow));
+    }
+
+    onSaveBtnClick() {
+        this.html.root.classList.remove("_changed");
+    }
+    onCancelBtnClick() {
+        this.html.root.classList.remove("_changed");
+    }
+
+    onAddBtnClick() { }
+
+    onSelectProgram(row: Row) {
+        this.html.root.classList.add("_changed");
+        row.value.program = (event as CustomEvent).detail.value;
+        row.value.program_id = row.value.program.id;
+        row.html.programDurationValue.innerText = this.convertDuration(row.value.program.duration).str;
+    }
+
+    onOrderPlusBtnClick(row: Row) {
+        const rowArrIdx = this.rows.findIndex((item) => item.value.id === row.value.id);
+
+        if (rowArrIdx < this.rows.length - 2) {
+            row.html.root.parentElement.insertBefore(row.html.root, this.rows[rowArrIdx + 2].html.root);
+            console.log(this.rows[rowArrIdx + 2].html.root)
+        } else if (rowArrIdx < this.rows.length - 1) {
+            row.html.root.parentElement.appendChild(row.html.root);
+        }
+
+        if (rowArrIdx < this.rows.length - 1) {
+            this.html.root.classList.add("_changed");
+            const nextRow = this.rows[rowArrIdx + 1];
+            this.rows[rowArrIdx + 1] = row;
+            this.rows[rowArrIdx] = nextRow;
+            row.value.order++;
+            nextRow.value.order--;
+            this.rowTimePlus(row, nextRow.value.program.duration);
+            this.rowTimeMinus(nextRow, row.value.program.duration);
+        }
+    }
+
+    onOrderMinusBtnClick(row: Row) {
+        const rowArrIdx = this.rows.findIndex((item) => item.value.id === row.value.id);
+
+        if (rowArrIdx > 0) {
+            this.html.root.classList.add("_changed");
+            const previousRow = this.rows[rowArrIdx - 1];
+            row.html.root.parentElement.insertBefore(row.html.root, previousRow.html.root);
+            this.rows[rowArrIdx - 1] = row;
+            this.rows[rowArrIdx] = previousRow;
+            row.value.order--;
+            previousRow.value.order++;
+            this.rowTimeMinus(row, previousRow.value.program.duration);
+            this.rowTimePlus(previousRow, row.value.program.duration);
+        }
+    }
+
+    onDeleteBtnClick(row: Row) {
+        // if (!confirm("Хорошо подумали?")) return;
+        // const row = getButtonsRow(event.currentTarget as HTMLButtonElement);
+        // const rowElems = getRowElems(row);
+        // await fetch(backendAddress + "/api/advertising/delete.php?id=" + rowElems.id.innerText, { method: "delete" });
+        // advertisings = advertisings.filter((item) => item.id !== +rowElems.id.innerText);
+        // const rowOrder = +row.dataset.order;
+        // row.parentElement.removeChild(row);
+        // for (let i = 0; i < rows.length; i++) if (+rows[i].dataset.order > rowOrder) {
+        //     rows[i].dataset.order = +rows[i].dataset.order - 1 + "";
+        // }
+        // for (let item of advertisings) if (item.order > rowOrder) item.order--;
+    }
+
+    convertDuration(duration: number) {
+        const hours = Math.floor(duration / 3600000);
+        const mins = Math.floor((duration - hours * 3600000) / 60000);
+        const str = (hours > 0 ? hours + "ч. " : "") + (mins > 0 || hours === 0 ? mins + "мин." : "");
+        return { hours, mins, str }
+    }
+
+    getOrdersTime() {
+        const time = {
+            hours: 0,
+            minutes: 0
+        };
+        for (let row of this.rows) {
+            row.html.timeValue.innerText = `${(time.hours < 10 ? "0" : "") + time.hours}:${(time.minutes < 10 ? "0" : "") + time.minutes}`;
+            time.minutes += Math.round(row.value.program.duration / 60000);
+            while (time.minutes > 59) {
+                time.minutes -= 60;
+                time.hours++;
+            }
+        }
+    }
+
+    rowTimePlus(row: Row, duration: number) {
+        const timeArr = row.html.timeValue.innerText.split(":");
+        const time = {
+            hours: +timeArr[0],
+            minutes: +timeArr[1]
+        };
+        time.minutes += Math.round(duration / 60000);
+        while (time.minutes > 59) {
+            time.minutes -= 60;
+            time.hours++;
+        }
+        row.html.timeValue.innerText = `${(time.hours < 10 ? "0" : "") + time.hours}:${(time.minutes < 10 ? "0" : "") + time.minutes}`;
+    }
+
+    rowTimeMinus(row: Row, duration: number) {
+        const timeArr = row.html.timeValue.innerText.split(":");
+        const time = {
+            hours: +timeArr[0],
+            minutes: +timeArr[1]
+        };
+        time.minutes -= Math.round(duration / 60000);
+        while (time.minutes < 0) {
+            time.minutes += 60;
+            time.hours--;
+        }
+        row.html.timeValue.innerText = `${(time.hours < 10 ? "0" : "") + time.hours}:${(time.minutes < 10 ? "0" : "") + time.minutes}`;
     }
 }
 
-function getButtonsRow(button: HTMLButtonElement) {
-    return (button.parentElement as HTMLElement).parentElement as HTMLElement;
-}
+new ScheldulePage();
 
-function addEventListenersToRow(row: HTMLElement) {
-    const rowElems = getRowElems(row);
-    new InputFind(
-        rowElems.programNameChangeElem,
-        programs,
-        programs.find((item) => item.id === +row.dataset.programId),
-        "name"
-    );
-    rowElems.programNameChangeElem.addEventListener("change", () => {
-        comp.classList.add("_changed");
-        rowElems.programDurationValue.innerText = convertDuration(JSON.parse(rowElems.programNameChangeElem.dataset.value).duration).str;
-    });
-    rowElems.btnDelete.addEventListener("click", onDeleteBtnClick);
-    rowElems.btnOrderPlus.addEventListener("click", onOrderPlusBtnClick);
-    rowElems.btnOrderPlus.addEventListener("click", onOrderPlusBtnClick);
-}
-
-function convertDuration(duration: number) {
-    const hours = Math.floor(duration / 3600000);
-    const mins = Math.floor((duration - hours * 3600000) / 60000);
-    const str = (hours > 0 ? hours + "ч. " : "") + (mins > 0 || hours === 0 ? mins + "мин." : "");
-    return { hours, mins, str }
-}
-
-function createRow(schelduleItem: Scheldule) {
-    const duration = convertDuration(schelduleItem.programDuration);
-    return `<div class="table__row" data-scheldule-id="${schelduleItem.id}" data-program-id="${schelduleItem.programId}">
-        <div class="table__content-cells-container">
-            <div class="table__cell _time">
-                <div class="table__cell-value">${""}</div>
-            </div>
-            <div class="table__cell _program-name">
-                <div class="table__cell-change-elem">${schelduleItem.programName}</div>
-            </div>
-            <div class="table__cell _program-duration">
-                <div class="table__cell-value">${duration.str}</div>
-            </div>
-        </div>
-        <div class="table__cell _btns">
-            <button class="pi pi-arrow-up table__btn _order-minus"></button>
-            <button class="pi pi-arrow-down table__btn _order-plus"></button>
-            <button class="pi pi-trash table__btn _delete"></button>
-        </div>
-    </div>`;
-}
-
-function onOrderPlusBtnClick(event: MouseEvent) {
-    // const row = getButtonsRow(event.currentTarget as HTMLButtonElement);
-    // if (row.nextElementSibling) {
-    //     row.parentElement.insertBefore(row, row.nextElementSibling.nextElementSibling);
-    // }
-}
-
-function onOrderMinusBtnClick(event: MouseEvent) {
-    // const row = getButtonsRow(event.currentTarget as HTMLButtonElement);
-    // if (row.previousElementSibling) {
-    //     row.parentElement.insertBefore(row, row.previousElementSibling);
-    // }
-}
-
-async function onDeleteBtnClick(event: MouseEvent) {
-    // if (!confirm("Хорошо подумали?")) return;
-    // const row = getButtonsRow(event.currentTarget as HTMLButtonElement);
-    // const rowElems = getRowElems(row);
-    // await fetch(backendAddress + "/api/advertising/delete.php?id=" + rowElems.id.innerText, { method: "delete" });
-    // advertisings = advertisings.filter((item) => item.id !== +rowElems.id.innerText);
-    // const rowOrder = +row.dataset.order;
-    // row.parentElement.removeChild(row);
-    // for (let i = 0; i < rows.length; i++) if (+rows[i].dataset.order > rowOrder) {
-    //     rows[i].dataset.order = +rows[i].dataset.order - 1 + "";
-    // }
-    // for (let item of advertisings) if (item.order > rowOrder) item.order--;
-}
-
-function onSaveBtnClick(event: MouseEvent) {
-    comp.classList.remove("_changed");
-}
-function onCancelBtnClick(event: MouseEvent) {
-    comp.classList.remove("_changed");
-}
-function onAddBtnClick() { }
-
-class Scheldule {
+class SchelduleItem {
     id: number;
     date: number;
     order: number;
-
-    programId: number;
-    programName: string;
-    programDuration: number;
+    program_id: number;
+    program?: Program;
 }
 
 class Program {
@@ -149,4 +240,17 @@ class Program {
     name: string;
     duration: number;
     link: string;
+}
+
+class Row {
+    html: {
+        root: HTMLElement;
+        timeValue: HTMLElement;
+        programNameChangeElem: HTMLElement;
+        programDurationValue: HTMLElement;
+        btnOrderPlus: HTMLButtonElement;
+        btnOrderMinus: HTMLButtonElement;
+        btnDelete: HTMLButtonElement;
+    };
+    value: SchelduleItem;
 }
